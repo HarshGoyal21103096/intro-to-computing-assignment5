c612bd3537a3b0d5008e30d469ddb2c3e65d68d4
HarshGoyal21103096 <97400616+HarshGoyal21103096@users.noreply.github.com>
2022-03-08 17:19:32 +0530

    Add files via upload

---
+++ assign5_sol4,5.py
@@ -0,0 +1,24 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: F:/python assignments/assign5_ques4,5.py ==============
+Question 4
+enter the number of integers to be entered:
+4
+Enter the integers with spaces but without commas:
+23 16 25 37
+Sorted array is:  [16, 23, 25, 37]
+
+
+
+
+Question 5
+enter the number of integers to be entered:
+12
+Enter the integers with space but without commas:
+5 7 16 17 45 13 19 12 4 12 12 15
+Sorted array is: [4, 5, 7, 12, 12, 12, 13, 15, 16, 17, 19, 45]
+Enter the integer from list whose index and number of occurances you want to find:
+12
+Value is at index: 4
+Total number of occurances is: 3
